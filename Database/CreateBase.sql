abe35f9630bc6979e57e7e05c3a67dbbc0d87d44
LucLop <lo.luas@outlook.com>
2019-07-18 21:47:40 +0200

    Table creation2

--- Database/CreateBase.sql
+++ Database/CreateBase.sql
@@ -1,40 +1,62 @@
-CREATE TABLE Order (
-    Product int,
-    Id int PRIMARY KEY,
-    Duration double,
-    Price double,
+CREATE TABLE Allergen (
+    Name varchar,
+    AllergenId int PRIMARY KEY,
 );
 
 CREATE TABLE Category (
-    Id int PRIMARY KEY,
+    CategoryId int PRIMARY KEY,
     Name varchar,
+);
+
+CREATE TABLE Order (
+    OrderId int PRIMARY KEY,
+    Duration double,
+    Price double,
 );
 
 CREATE TABLE Product (
     Name varchar,
     Price double,
-    Ingredient int,
-    Id int PRIMARY KEY,
+    ProductId int PRIMARY KEY,
     Duration double,
     Category int,
+    CONSTRAINT FOREIGN KEY (OrderId) REFERENCES (Order.OrderId),
 );
 
 CREATE TABLE Ingredient (
     Name varchar,
-    Allergen int,
-    Id int PRIMARY KEY,
-);
-
-CREATE TABLE Allergen (
-    Name varchar,
-    Id int PRIMARY KEY,
+    IngredientId int PRIMARY KEY,
 );
 
 CREATE TABLE Stock (
-    Ingredient int,
+    IngredientId int PRIMARY KEY,
     Quantity int,
 );
 
-    
+CREATE TABLE ProductOrder (
+    OrderId int,
+    Product int,
+    quantity int,
+    CONSTRAINT PRIMARY KEY (OrderId, CategoryId),
+    CONSTRAINT FOREIGN KEY (OrderId) REFERENCES (Order.OrderId),
+    CONSTRAINT FOREIGN KEY (ProductId) REFERENCES (Product.ProductId),
+);  
+
+
+CREATE TABLE IngredientProduct (
+    IngredientId int,
+    ProductId int,
+    CONSTRAINT PRIMARY KEY (IngredientId, ProductId),
+    CONSTRAINT FOREIGN KEY (IngredientId) REFERENCES (Ingredient.IngredientId),
+    CONSTRAINT FOREIGN KEY (ProductId) REFERENCES (Product.ProductId),
+);
+
+CREATE TABLE AllergenIngredient (
+    AllergenId int,
+    IngredientId int,
+    CONSTRAINT PRIMARY KEY (AllergenId, IngredientId),
+    CONSTRAINT FOREIGN KEY (AllergenId) REFERENCES (Allergen.AllergenId),
+    CONSTRAINT FOREIGN KEY (IngredientId) REFERENCES (Ingredient.IngredientId),
+);
         
     
