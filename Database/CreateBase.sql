eb0742f1bf109b8af74aca258231b681011a8b31
LucLop <lo.luas@outlook.com>
2019-07-18 22:21:52 +0200

    Tables clean

--- Database/CreateBase.sql
+++ Database/CreateBase.sql
@@ -1,75 +1,63 @@
 CREATE DATABASE OrderPizza;
 
 CREATE TABLE Allergen (
-    Name varchar,
-    AllergenId int PRIMARY KEY,
+    Name varchar(80),
+    AllergenId int PRIMARY KEY AUTO_INCREMENT
 );
 
 CREATE TABLE Category (
-    CategoryId int PRIMARY KEY,
-    Name varchar,
+    CategoryId int PRIMARY KEY AUTO_INCREMENT,
+    Name varchar(80)
 );
 
 CREATE TABLE Ingredient (
-    Name varchar,
-    IngredientId int PRIMARY KEY,
+    Name varchar(80),
+    IngredientId int PRIMARY KEY AUTO_INCREMENT
 );
 
 CREATE TABLE AllergenIngredient (
     AllergenId int,
     IngredientId int,
     CONSTRAINT PRIMARY KEY (AllergenId, IngredientId),
-    CONSTRAINT FOREIGN KEY (AllergenId) REFERENCES (Allergen.AllergenId),
-    CONSTRAINT FOREIGN KEY (IngredientId) REFERENCES (Ingredient.IngredientId),
+    CONSTRAINT FOREIGN KEY (AllergenId) REFERENCES Allergen(AllergenId),
+    CONSTRAINT FOREIGN KEY (IngredientId) REFERENCES Ingredient(IngredientId)
 );
 
 CREATE TABLE Stock (
-    IngredientId int PRIMARY KEY,
+    IngredientId int PRIMARY KEY AUTO_INCREMENT,
     Quantity int,
-    CONSTRAINT FOREIGN KEY (IngredientId) REFERENCES (Ingredient.IngredientId),
+    CONSTRAINT FOREIGN KEY (IngredientId) REFERENCES Ingredient(IngredientId)
 );
 
 CREATE TABLE Product (
-    Name varchar,
+    Name varchar(80),
     Price double,
-    ProductId int PRIMARY KEY,
+    ProductId int PRIMARY KEY AUTO_INCREMENT,
     Duration double,
     CategoryId int,
-    CONSTRAINT FOREIGN KEY (CategoryId) REFERENCES (Category.CategoryId),
+    CONSTRAINT FOREIGN KEY (CategoryId) REFERENCES Category(CategoryId)
 );
 
 CREATE TABLE IngredientProduct (
     IngredientId int,
     ProductId int,
     CONSTRAINT PRIMARY KEY (IngredientId, ProductId),
-    CONSTRAINT FOREIGN KEY (IngredientId) REFERENCES (Ingredient.IngredientId),
-    CONSTRAINT FOREIGN KEY (ProductId) REFERENCES (Product.ProductId),
+    CONSTRAINT FOREIGN KEY (IngredientId) REFERENCES Ingredient(IngredientId),
+    CONSTRAINT FOREIGN KEY (ProductId) REFERENCES Product(ProductId)
 );
 
-CREATE TABLE Order (
-    OrderId int PRIMARY KEY,
+CREATE TABLE Command (
+    CommandId int PRIMARY KEY AUTO_INCREMENT,
     Duration double,
-    Price double,
+    Price double
 );
 
 CREATE TABLE ProductOrder (
-    OrderId int,
-    Product int,
-    quantity int,
-    CONSTRAINT PRIMARY KEY (OrderId, CategoryId),
-    CONSTRAINT FOREIGN KEY (OrderId) REFERENCES (Order.OrderId),
-    CONSTRAINT FOREIGN KEY (ProductId) REFERENCES (Product.ProductId),
+    CommandId int,
+    ProductId int,
+    Quantity int,
+    CONSTRAINT PRIMARY KEY (CommandId, ProductId),
+    CONSTRAINT FOREIGN KEY (CommandId) REFERENCES Command(CommandId),
+    CONSTRAINT FOREIGN KEY (ProductId) REFERENCES Product(ProductId)
 );  
 
-
-
-
-
-
-
-
-
-
-
-        
-    
