3bbd19e9042d983d7bdff6b2b1813486ff9726a4
lm173618 <leandro.maouchi@gmail.com>
2019-07-18 21:23:36 +0100

    Main Menu

--- Desktop/Program.cs
+++ Desktop/Program.cs
@@ -17,6 +17,7 @@ namespace Desktop
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //throw new NotImplementedException();
         }
     }
 }
